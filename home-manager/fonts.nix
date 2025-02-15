ad6e779cf858cb57e6ef1b299f60a853524e77f6
Pxndxs 🐼 <pxnditxyr@gmail.com>
2025-02-15 12:49:59 -0400

    chore: ⬆️📦♻️ update Nix flake to upgrade all packages to their latest versions chore: 🎨🔄✨ update fonts to the new version for improved visual consistency

--- home-manager/fonts.nix
+++ home-manager/fonts.nix
@@ -1,10 +1,19 @@
 { pkgs, ... }: {
   fonts.fontconfig.enable = true;
 
+  # home.packages = with pkgs; [
+  #   victor-mono
+  #   (nerdfonts.override { fonts = [ "JetBrainsMono" "CascadiaMono" "CascadiaCode" "NerdFontsSymbolsOnly" ]; })
+  #   noto-fonts-emoji
+  #   powerline-symbols
+  # ];
   home.packages = with pkgs; [
     victor-mono
-    (nerdfonts.override { fonts = [ "JetBrainsMono" "CascadiaMono" "CascadiaCode" "NerdFontsSymbolsOnly" ]; })
-    noto-fonts-emoji
+    nerd-fonts.jetbrains-mono
+    nerd-fonts.caskaydia-cove
+    nerd-fonts.caskaydia-mono
+    nerd-fonts.symbols-only
+    noto-fonts-color-emoji
     powerline-symbols
   ];
 }
