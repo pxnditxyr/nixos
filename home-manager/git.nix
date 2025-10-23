cb4ded9d3eaf12f448c29fc798135362186f9189
Pxndxs 🐼 <pxnditxyr@gmail.com>
2025-10-23 10:50:21 -0400

    chore: ⬆️🛠️💻 Update **Home Manager** and **Nix core** from **24.11** to **25.11** fix: 🐛🛠️💻 Fix an **alias error** in the **Git configuration** within Nix chore: ⬆️🛠️💻 Update the **Nix flake**

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -6,11 +6,11 @@
         name = "Pxndxs 🐼";
         email = "pxnditxyr@gmail.com";
       };
-      aliases = {
+      init.defaultBranch = "main";
+      alias = {
         s = "status -sb";
         lg = ''log --graph --abbrev-commit --decorate --pretty="format:%C(bold yellow)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(italic cyan)%s%C(reset) %C(cyan)- %an%C(reset)%C(bold yellow)%d%C(reset)" --all'';
       };
-      init.defaultBranch = "main";
     };
   };
 }
