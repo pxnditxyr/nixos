211969ab166f6007e5068440e57e063d5a77115e
Pxndxs 🐼 <pxnditxyr@gmail.com>
2026-05-10 13:10:28 -0400

    chore(hardware): 🖥️ update disk UUIDs and system config for new machine 🚀
    ✨ Transitioned to a new hardware setup!
    🛠️ Updated root (/) and boot (/boot) partition UUIDs to match the new
    physical drives 💾.
    🧹 Removed legacy `kvm.enable_virt_at_load` kernel parameters.
    🌐 Cleaned up explicit DHCP networking defaults to rely on the new
    system's auto-configuration.

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -11,31 +11,22 @@
   boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
-  boot.kernelParams = [ "kvm.enable_virt_at_load=0" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/bc7859ba-720c-4f97-9228-e0ef307a94cf";
+    { device = "/dev/disk/by-uuid/9b1e90cc-f341-43a5-8bc6-822cf22d6ee9";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/9031-3C97";
+    { device = "/dev/disk/by-uuid/64E2-7529";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
   swapDevices =
-    [ { device = "/dev/disk/by-uuid/ea06cd21-41d0-448b-94c8-06feaada8401"; }
+    [ { device = "/dev/disk/by-uuid/833d4246-f7f6-425f-bd74-519a0e68a238"; }
     ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
